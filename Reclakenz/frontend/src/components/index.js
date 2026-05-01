37d5dec41187f891a443257b3d99cd69b79c1ceb
Ayman-dahraoui <aymandahraoui18@gmail.com>
2026-05-01 02:02:45 +0100

    Revert "refactor:move scripts to scripts folder"
    This reverts commit 9974e088fa863f39f440f67a5a530306b64840b2.

--- Reclakenz/frontend/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as PageLayout } from './PageLayout';
-export { default as ProfessionalCard } from './ProfessionalCard';
-export { default as ProfessionalButton } from './ProfessionalButton';
-export { default as ProfessionalTable } from './ProfessionalTable';
-export { default as DashboardStats } from './DashboardStats'; 
